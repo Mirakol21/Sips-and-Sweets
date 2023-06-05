1f1122cae11517d09ae01117f6e045db487f2272
Mirakol21 <119095222+Mirakol21@users.noreply.github.com>
2023-06-05 10:56:11 +0800

    Add files via upload

---
+++ Assignment/Assignment Coding.js
@@ -0,0 +1,30 @@
+function input() {
+
+    let num = document.getElementById("nm1").value;
+
+if  (num == 1) {
+    alert("One");
+} else if (num == 2) {
+    alert("Two");
+} else if (num == 3) {
+    alert("Three");
+} else if (num == 4) {
+    alert("Four");
+} else if (num == 5) {
+    alert("Five");
+} else if (num == 6) {
+    alert("Six");
+} else if (num == 7) {
+    alert("Seven");
+} else if (num == 8) {
+    alert("Eight");
+} else if (num == 9) {
+    alert("Nine");
+} else if (num == 10) {
+    alert("Ten");
+} else {
+    alert("Invalid number, Enter 1-10 only")
+}
+
+}
+
